b93a07710c8bb623663cf6c5787b6879353918d8
Devine Lu Linvega <aliceffekt@gmail.com>
2016-10-16 10:30:47 -0700

    Working copy

--- vessel.rb
+++ vessel.rb
@@ -5,61 +5,63 @@ class Disms
 
   include Vessel
 
-  def word
+  class PassiveActions
 
-    timeDiff  = Time.new.to_i - Date.new(2015,8,21).to_time.to_i
-    daysGone  = (timeDiff/86400)
-    chapter   = (daysGone * 24) + Time.now.hour
+    include ActionCollection
 
-    word = dict["NOUN"][chapter % dict["NOUN"].length]
+    def answer q = "Home"
 
-    # -te
-    if word[-2,2] == "te"
-      word = word[0...-1]+"ism"
-    # -ve
-    elsif word[-2,2] == "ve"
-      word = word[0...-1]+"ism"
-    # -ly
-    elsif word[-2,2] == "ly"
-      word = word+"ism"   
-    # -ed
-    elsif word[-2,2] == "ed"
-      word = word[0...-2]+"ism"
-    # -ist
-    elsif word[-3,3] == "ist"
-      word = word[0...-3]+"ism"
-    # -y
-    elsif word[-1,1] == "y"
-      word = word[0...-1]+"ism"
-    else
-      word += "ism"
-    end
+      timeDiff  = Time.new.to_i - Date.new(2015,8,21).to_time.to_i
+      daysGone  = (timeDiff/86400)
+      chapter   = (daysGone * 24) + Time.now.hour
 
-    return word.capitalize
+      word_type = ""
 
-  end
+      dict = {}
 
-  def dict
-
-    word_type = ""
-
-    dict = {}
-
-    File.open("#{$nataniev.path}/library/dictionary.en","r:UTF-8") do |f|
-      f.each_line do |line|
-        depth = line[/\A */].size
-        line = line.strip
-        if depth == 0
-          word_type = line
-        else
-          if !dict[word_type] then dict[word_type] = [] end
-          dict[word_type].push(line)
+      File.open("#{$nataniev.path}/library/dictionary.en","r:UTF-8") do |f|
+        f.each_line do |line|
+          depth = line[/\A */].size
+          line = line.strip
+          if depth == 0
+            word_type = line
+          else
+            if !dict[word_type] then dict[word_type] = [] end
+            dict[word_type].push(line)
+          end
         end
       end
+      
+      word = dict["NOUN"][chapter % dict["NOUN"].length]
+
+      # -te
+      if word[-2,2] == "te"
+        word = word[0...-1]+"ism"
+      # -ve
+      elsif word[-2,2] == "ve"
+        word = word[0...-1]+"ism"
+      # -ly
+      elsif word[-2,2] == "ly"
+        word = word+"ism"   
+      # -ed
+      elsif word[-2,2] == "ed"
+        word = word[0...-2]+"ism"
+      # -ist
+      elsif word[-3,3] == "ist"
+        word = word[0...-3]+"ism"
+      # -y
+      elsif word[-1,1] == "y"
+        word = word[0...-1]+"ism"
+      else
+        word += "ism"
+      end
+
+      return word.capitalize
+
     end
 
-    return dict
-
   end
+  
+  def passive_actions ; return PassiveActions.new(self,self) end
 
 end
