5028ab5a4f822120827e1840f9bdbc4bf9bfbe3a
Devine Lu Linvega <aliceffekt@gmail.com>
2016-10-20 11:48:33 -0700

    Rebuilt auto/tweet

--- vessel.rb
+++ vessel.rb
@@ -1,67 +1,82 @@
 #!/bin/env ruby
 # encoding: utf-8
 
+$instance_path = File.expand_path(File.join(File.dirname(__FILE__), "/"))
+
 class Disms
 
   include Vessel
 
-  class PassiveActions
+  def twitter_account
 
-    include ActionCollection
+    return "dictionarism"
 
-    def answer q = "Home"
+  end
 
-      timeDiff  = Time.new.to_i - Date.new(2015,8,21).to_time.to_i
-      daysGone  = (timeDiff/86400)
-      chapter   = (daysGone * 24) + Time.now.hour
+  def make_ism
 
-      word_type = ""
+    timeDiff  = Time.new.to_i - Date.new(2015,8,21).to_time.to_i
+    daysGone  = (timeDiff/86400)
+    chapter   = (daysGone * 24) + Time.now.hour
 
-      dict = {}
+    word_type = ""
 
-      File.open("#{$nataniev.path}/library/dictionary.en","r:UTF-8") do |f|
-        f.each_line do |line|
-          depth = line[/\A */].size
-          line = line.strip
-          if depth == 0
-            word_type = line
-          else
-            if !dict[word_type] then dict[word_type] = [] end
-            dict[word_type].push(line)
-          end
+    dict = {}
+
+    File.open("#{$nataniev.path}/library/dictionary.en","r:UTF-8") do |f|
+      f.each_line do |line|
+        depth = line[/\A */].size
+        line = line.strip
+        if depth == 0
+          word_type = line
+        else
+          if !dict[word_type] then dict[word_type] = [] end
+          dict[word_type].push(line)
         end
       end
-      
-      word = dict["NOUN"][chapter % dict["NOUN"].length]
+    end
+    
+    word = dict["NOUN"][chapter % dict["NOUN"].length]
 
-      # -te
-      if word[-2,2] == "te"
-        word = word[0...-1]+"ism"
-      # -ve
-      elsif word[-2,2] == "ve"
-        word = word[0...-1]+"ism"
-      # -ly
-      elsif word[-2,2] == "ly"
-        word = word+"ism"   
-      # -ed
-      elsif word[-2,2] == "ed"
-        word = word[0...-2]+"ism"
-      # -ist
-      elsif word[-3,3] == "ist"
-        word = word[0...-3]+"ism"
-      # -y
-      elsif word[-1,1] == "y"
-        word = word[0...-1]+"ism"
-      else
-        word += "ism"
-      end
+    # -te
+    if word[-2,2] == "te"
+      word = word[0...-1]+"ism"
+    # -ve
+    elsif word[-2,2] == "ve"
+      word = word[0...-1]+"ism"
+    # -ly
+    elsif word[-2,2] == "ly"
+      word = word+"ism"   
+    # -ed
+    elsif word[-2,2] == "ed"
+      word = word[0...-2]+"ism"
+    # -ist
+    elsif word[-3,3] == "ist"
+      word = word[0...-3]+"ism"
+    # -y
+    elsif word[-1,1] == "y"
+      word = word[0...-1]+"ism"
+    else
+      word += "ism"
+    end
 
-      return word.capitalize
+    return word.capitalize
+
+  end
+
+  # Actions
+
+  class Actions
+
+    include ActionCollection
+    include ActionTweet
+
+    def tweet_auto
+
+      return tweet(@actor.make_ism)
 
     end
 
   end
-  
-  def passive_actions ; return PassiveActions.new(self,self) end
 
 end
