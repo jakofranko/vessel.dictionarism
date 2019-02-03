9750e58d9952dc112be0ab87ad379f2b3a2363a7
Jake Franklin <jacob.d.franklin@gmail.com>
2019-02-03 15:05:31 -0700

    whitespace

--- vessel.rb
+++ vessel.rb
@@ -25,75 +25,75 @@ end
 
 class ActionGenerate
 
-  include Action
+    include Action
 
-  def initialize q = nil
+    def initialize q = nil
 
-    super
+        super
 
-    @name = "Generate"
-    @docs = "Generate a new -ism"
+        @name = "Generate"
+        @docs = "Generate a new -ism"
 
-  end
-
-  def act q = nil
-
-    timeDiff  = Time.new.to_i - Date.new(2015,8,21).to_time.to_i
-    daysGone  = (timeDiff/86400)
-    chapter   = (daysGone * 24) + Time.now.hour
-
-    word_type = ""
-
-    dict = {}
-
-    Memory_Array.new("dictionary").to_a.each do |line|
-      word_type = line['C']
-      if !dict[word_type] then dict[word_type] = [] end
-      dict[word_type].push(line['WORD'])
     end
 
-    word = q.to_s != "" ? q : dict["N"][chapter % dict["N"].length]
+    def act q = nil
 
-    # -te
-    if word[-2,2] == "te"
-      word = word[0...-1]+"ism"
-    # -ve
-    elsif word[-2,2] == "ve"
-      word = word[0...-1]+"ism"
-    # -ly
-    elsif word[-2,2] == "ly"
-      word = word+"ism"
-    # -ed
-    elsif word[-2,2] == "ed"
-      word = word[0...-2]+"ism"
-    # -ist
-    elsif word[-3,3] == "ist"
-      word = word[0...-3]+"ism"
-    # -y
-    elsif word[-1,1] == "y"
-      word = word[0...-1]+"ism"
-    else
-      word += "ism"
+        timeDiff  = Time.new.to_i - Date.new(2015,8,21).to_time.to_i
+        daysGone  = (timeDiff/86400)
+        chapter   = (daysGone * 24) + Time.now.hour
+
+        word_type = ""
+
+        dict = {}
+
+        Memory_Array.new("dictionary").to_a.each do |line|
+            word_type = line['C']
+            if !dict[word_type] then dict[word_type] = [] end
+            dict[word_type].push(line['WORD'])
+        end
+
+        word = q.to_s != "" ? q : dict["N"][chapter % dict["N"].length]
+
+        # -te
+        if word[-2,2] == "te"
+            word = word[0...-1]+"ism"
+        # -ve
+        elsif word[-2,2] == "ve"
+            word = word[0...-1]+"ism"
+        # -ly
+        elsif word[-2,2] == "ly"
+            word = word+"ism"
+        # -ed
+        elsif word[-2,2] == "ed"
+            word = word[0...-2]+"ism"
+        # -ist
+        elsif word[-3,3] == "ist"
+            word = word[0...-3]+"ism"
+        # -y
+        elsif word[-1,1] == "y"
+            word = word[0...-1]+"ism"
+        else
+            word += "ism"
+        end
+
+        return word.capitalize
+
     end
-
-    return word.capitalize
-
-  end
 
 end
 
 class ActionTweet
 
-  def account
+    def account
 
-    return "dictionarism"
+        return "dictionarism"
 
-  end
+    end
 
-  def payload
+    def payload
 
-    return ActionGenerate.new(@host).act
+        return ActionGenerate.new(@host).act
 
-  end
+    end
 
 end
